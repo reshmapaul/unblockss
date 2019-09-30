224f6cbbddb14aaad3d240a413bcc0e98f535120
Benson A P <benson.ap@citrusinformatics.com>
2019-09-30 18:19:37 +0530

    New section added after hero image section, icons added for new section and services, Style modified
    New section added after hero image section and content added, icons added for new and services. Formspree form added as a contact form, modified style.

--- static/js/main.js
+++ static/js/main.js
@@ -50,8 +50,107 @@ $(document).ready(function(){
         }
     );
 
+
+
     //contact form validation
-    $("#contact-form").validate({
+    function uuidv4() {
+      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      )
+    }
+   var uid = uuidv4();
+   //alert(uid);
+   $('#contact-submit-live').click(function(e){
+      var first_name = $('#name').val();
+      var email = $('#email').val();
+      var subject = $('#subject').val();
+      var message = $('#message').val();
+      var o = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        if (first_name == '') {
+          $('#namealert').css('display', 'block');
+              return  false;
+         }
+         else {
+          $('#namealert').css('display', 'none');
+         }
+         if (email == '' || email.search(o) == -1 ) {
+            $('#emailalert').css('display', 'block');
+            return  false;
+         }
+         else {
+            $('#emailalert').css('display', 'none');
+          }
+         if (subject == '') {
+          $('#subjectalert').css('display', 'block');
+            return  false;
+         }
+         else {
+          $('#subjectalert').css('display', 'none');
+         }
+         
+         
+          var form = new FormData();
+          form.append("grant_type", "client_credentials");
+          form.append("client_id", "edcca281-18eb-1d06-a1a8-5d1c45897769");
+          form.append("client_secret", "citrus");
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://10.10.10.134:82/Api/access_token",
+            "method": "POST",
+            "headers": {
+              "Accept": "application/vnd.api+json"
+            },
+            "processData": false,
+            "contentType": false,
+            "mimeType": "multipart/form-data",
+            "data": form
+          }
+
+      $.ajax(settings).done(function (response) {
+      var obj = $.parseJSON(response);
+      var access_token= obj.access_token;
+      var settings = {
+          "url": "http://10.10.10.134:82/Api/V8/module",
+          "method": "POST",
+          "headers": {
+            "Accept": "application/vnd.api+json",
+            "Authorization": "Bearer "+access_token+"",
+            "Content-Type": "application/json"
+           },
+          "processData": false,
+          "data": "{\r\n  \"data\": {\r\n    \"type\": \"Contacts\",\r\n    \"id\": \""+uid+ "\",\r\n    \"attributes\": {\r\n     \"first_name\":\""+first_name+"\",\r\n     \"email1\":\""+email+"\"\r\n,\r\n     \"description\":\""+message+"\"\r\n,\r\n     \"lead_source\":\"Web Site\"\r\n,\r\n     \"title\":\""+subject+"\"\r\n   }\r\n  }\r\n}\r\n"
+        }
+
+      $.ajax(settings).done(function (response) {
+      //console.log(response);
+          var contactid = response.data.id;
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://10.10.10.134:82/Api/V8/module/Accounts/4aac7119-308d-9c50-16ae-5d7b5c3ced70/relationships",
+            "method": "POST",
+            "headers": {
+              "Accept": "application/vnd.api+json",
+              "Authorization": "Bearer "+access_token+"",
+              "Content-Type": "application/json"
+            },
+            "processData": false,
+            "data": "{  \r\n   \"data\":{  \r\n         \"type\":\"Contacts\",\r\n         \"id\":\""+contactid+"\"\r\n\t    \r\n      }\r\n}"
+          }
+
+    $.ajax(settings).done(function (response) {
+         if(response.meta.message != ""){
+            var url="https://formspree.io/benson.ap@citrusinformatics.com"; 
+            $('#quickcontact').attr('action', url);
+            $('#quickcontact').submit();
+          }
+          });
+      });  
+    });      
+   
+        });
+    /*$("#contact-form").validate({
         rules: {
             name: {
                 required: true,
@@ -97,7 +196,7 @@ $(document).ready(function(){
                 }
             });
         }
-    });
+    });*/
 
 });
 
